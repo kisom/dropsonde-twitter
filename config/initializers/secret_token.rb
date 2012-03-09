1e00e30d495be1613d39d8d1fbf1cb5525d390f7
Kyle Isom <coder@kyleisom.net>
2012-03-09 11:02:20 +0300

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DropsondeTwitter::Application.config.secret_token = '437c28f898bd3dd0a16b9a6f56d4b0c29dce5b07c423836f5ad040b4547c5f0049a19a8f61aed3048519d24b4a74c2be14297b9274fc707fcad92ee063486f7e'
