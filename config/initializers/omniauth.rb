cb683c550a5b081f678878056a463cfa620f2e7e
Kyle Isom <coder@kyleisom.net>
2012-03-09 14:20:36 +0300

    fix missing vars in omniauth init

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 twitter_consumer_key = ENV['CONSUMER_KEY']
-twitter_consumer_key = ENV['CONSUMER_SECRET']
+twitter_consumer_secret = ENV['CONSUMER_SECRET']
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-      provider :twitter, 
+      provider :twitter, twitter_consumer_key, twitter_consumer_secret
 end
