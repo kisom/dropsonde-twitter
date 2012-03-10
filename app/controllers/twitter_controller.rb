5f156b3e56bded7ad53090ba7df566aa93cccc18
Kyle Isom <coder@kyleisom.net>
2012-03-10 13:24:28 +0300

    trying to get auth_token

--- app/controllers/twitter_controller.rb
+++ app/controllers/twitter_controller.rb
@@ -9,7 +9,8 @@ class TwitterController < ApplicationController
   # end
 
   def auth(callback = nil, auth = {})
-  	@auth_token = params[:query_string['oauth_token']]
+  	puts auth
+  	@auth_token = params[:oauth_token]
   end
 
 end
