116fde45b2fbf42e3db6b010a2d34e9d1a43c7be
Kyle Isom <coder@kyleisom.net>
2012-03-10 13:30:12 +0300

    now to get the auth secret

--- app/controllers/twitter_controller.rb
+++ app/controllers/twitter_controller.rb
@@ -9,8 +9,14 @@ class TwitterController < ApplicationController
   # end
 
   def auth(callback = nil, auth = {})
-  	puts auth
   	@auth_token = params[:oauth_token]
+  	@auth = auth
+  	# Twitter.configure do |config|
+  	# 	config.consumer_key = ENV['CONSUMER_KEY']
+  	# 	config.consumer_secret = ENV['CONSUMER_SECRET']
+  	# 	config.oauth_token = @auth_token
+  	# 	config.oauth_secret = @auth_secret
+  	# end
   end
 
 end
